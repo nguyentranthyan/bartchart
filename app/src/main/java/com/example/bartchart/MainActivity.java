8c5dc8a445d675066624701879a744cfccb56e20
nguyentranthyan <thyannguyentran@gmail.com>
2020-06-16 13:07:48 +0700

    Initial commit

---
+++ app/src/main/java/com/example/bartchart/MainActivity.java
@@ -0,0 +1,49 @@
+package com.example.bartchart;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
+import com.github.mikephil.charting.utils.ColorTemplate;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        BarChart barChart = (BarChart) findViewById(R.id.barchart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(8, 0));
+        entries.add(new BarEntry(2, 1));
+        entries.add(new BarEntry(5, 2));
+        entries.add(new BarEntry(20, 3));
+        entries.add(new BarEntry(15, 4));
+        entries.add(new BarEntry(19, 5));
+
+        BarDataSet bardataset = new BarDataSet(entries, "Cells");
+
+        ArrayList<String> labels = new ArrayList<String>();
+        labels.add("2016");
+        labels.add("2015");
+        labels.add("2014");
+        labels.add("2013");
+        labels.add("2012");
+        labels.add("2011");
+
+        BarData data = new BarData(labels, bardataset);
+        barChart.setData(data); // set the data and list of labels into chart
+        barChart.setDescription("Set Bar Chart Description Here");  // set the description
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+//        barChart.animateY(5000);
+
+    }
+}
